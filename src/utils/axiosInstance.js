f386f3a5b3214a4e7a036da44f6347d50c400ea6
rahisisoft <rahisisoft@gmail.com>
2025-01-29 17:07:44 +0200

    Initial commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: "https://retfarmer.org/retfarmer-api/", // Change this to your PHP backend URL
-  baseURL: "http://localhost/agricore-api/",
+  baseURL: "https://retfarmer.org/retfarmer-api/", // Change this to your PHP backend URL
+  //baseURL: "http://localhost/agricore-api/",
   headers: { "Content-Type": "application/json" },
   withCredentials: true, // To handle cookies (if required)
 });
